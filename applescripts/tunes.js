a43a181d7881f42022e4ec1e4086a68fe6bebd97
Kleber Soares <kleberr@msn.com>
2020-11-20 09:18:00 -0300

    Fix display music

--- applescripts/tunes.js
+++ applescripts/tunes.js
@@ -1,21 +1,17 @@
 let output = "";
-if (Application("Music").running()) {
-    const track = Application("Music").currentTrack;
-    const artist = track.artist();
-    const title = track.name();
-    output = `${title} - ${artist}`.substr(0, 50);
-} else if (Application("iTunes").running()) {
+
+if (Application("iTunes").running()) {
     const track = Application("iTunes").currentTrack;
     const artist = track.artist();
     const title = track.name();
     output = `${title} - ${artist}`.substr(0, 50);
+    // } else if (Application("Music").running()) {
+    //     const track = Application("Music").currentTrack;
+    //     const artist = track.artist();
+    //     const title = track.name();
+    //     output = `${title} - ${artist}`.substr(0, 50);
 } else if (Application("Spotify").running()) {
     const track = Application("Spotify").currentTrack;
-    const artist = track.artist();
-    const title = track.name();
-    output = `${title} - ${artist}`.substr(0, 50);
-} else if (Application("Deezer").running()) {
-    const track = Application("Deezer").currentTrack;
     const artist = track.artist();
     const title = track.name();
     output = `${title} - ${artist}`.substr(0, 50);
